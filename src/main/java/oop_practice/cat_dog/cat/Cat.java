25e7b9b5bea8041d9ae6bca31bb57f5ef188510e
Beatriz Souza <122176237+biisouza@users.noreply.github.com>
2024-03-08 17:08:45 +0000

    Update Cat.java

--- src/main/java/oop_practice/cat_dog/cat/Cat.java
+++ src/main/java/oop_practice/cat_dog/cat/Cat.java
@@ -1,5 +1,13 @@
 package oop_practice.cat_dog.cat;
 
-public class Cat {
+public class Cat extends Animal{
+  private String name,
 
+  public Cat(String name)  {
+        this.name = name;
+    }
+
+  public void sound() {
+       System.out.println("I'm a Cat. My name " + name + ". Mew");
+    }
 }
